b8c124b68b4590031cdcaaa6591cda57beffc7eb
yutianwu <wzxingbupt@gmail.com>
2024-06-25 14:27:57 +0800

    fix: fix lint error

--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -132,7 +132,7 @@ mod optimism {
     };
     #[cfg(feature = "opbnb")]
     pub use reth_chainspec::{
-        net::{opbnb_testnet_nodes, opbnb_mainnet_nodes},
+        net::{opbnb_mainnet_nodes, opbnb_testnet_nodes},
         OPBNB_MAINNET, OPBNB_TESTNET,
     };
 }
