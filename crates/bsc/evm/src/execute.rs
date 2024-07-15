428a1a1ed04882a7a3e02ce9ac8fb0b6e27b5ebc
Roshan <luoshen1997@gmail.com>
2024-07-15 14:58:54 +0800

    fix build error

--- crates/bsc/evm/src/execute.rs
+++ crates/bsc/evm/src/execute.rs
@@ -607,10 +607,10 @@ where
         if system_txs.is_empty() || hash != system_txs[0].signature_hash() {
             // slash tx could fail and not in the block
             if let Some(to) = transaction.to() {
-                if to == SLASH_CONTRACT.parse().unwrap() &&
+                if to == SLASH_CONTRACT.parse::<Address>().unwrap() &&
                     (system_txs.is_empty() ||
                         system_txs[0].to().unwrap_or_default() !=
-                            SLASH_CONTRACT.parse().unwrap())
+                            SLASH_CONTRACT.parse::<Address>().unwrap())
                 {
                     warn!("slash validator failed");
                     return Ok(());
