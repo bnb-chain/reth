66150f634abf5420517a6eefbef86e12ac60b13b
j75689 <j75689@gmail.com>
2024-10-08 15:01:26 +0800

    fix: lint error

--- crates/chainspec/src/spec.rs
+++ crates/chainspec/src/spec.rs
@@ -617,13 +617,9 @@ impl ChainSpec {
             C::Optimism => Some(op_nodes()),
             C::BaseGoerli | C::BaseSepolia => Some(base_testnet_nodes()),
             C::OptimismSepolia | C::OptimismGoerli | C::OptimismKovan => Some(op_testnet_nodes()),
-            #[cfg(feature = "bsc")]
             C::BinanceSmartChain => Some(bsc_mainnet_nodes()),
-            #[cfg(feature = "bsc")]
             C::BinanceSmartChainTestnet => Some(bsc_testnet_nodes()),
-            #[cfg(all(feature = "optimism", feature = "opbnb"))]
             C::OpBNBTestnet => Some(opbnb_testnet_nodes()),
-            #[cfg(all(feature = "optimism", feature = "opbnb"))]
             C::OpBNBMainnet => Some(opbnb_mainnet_nodes()),
             _ => None,
         }
