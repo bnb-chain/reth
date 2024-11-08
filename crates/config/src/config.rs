32afc58ea0e112364850ea3779d8dd86f6eb5b89
Roshan <luoshen1997@gmail.com>
2024-11-08 11:12:37 +0800

    chore: update the default `MerkleConfig`

--- crates/config/src/config.rs
+++ crates/config/src/config.rs
@@ -309,7 +309,7 @@ pub struct MerkleConfig {
 
 impl Default for MerkleConfig {
     fn default() -> Self {
-        Self { clean_threshold: 5_000 }
+        Self { clean_threshold: 50_000 }
     }
 }
 
