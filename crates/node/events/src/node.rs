a69b74d66fef6f942c5e98bf6d81df576ce77891
qybdyx <qybdyx@outlook.com>
2025-10-29 17:17:30 +0800

    feat: setup reth si test 2

--- crates/node/events/src/node.rs
+++ crates/node/events/src/node.rs
@@ -227,7 +227,7 @@ impl NodeState {
                             "Received forkchoice updated message when syncing"
                         }
                     };
-                    info!(?head_block_hash, ?safe_block_hash, ?finalized_block_hash, "{}", msg);
+                    // info!(?head_block_hash, ?safe_block_hash, ?finalized_block_hash, "{}", msg);
                 }
                 self.head_block_hash = Some(head_block_hash);
                 self.safe_block_hash = Some(safe_block_hash);
@@ -279,7 +279,7 @@ impl NodeState {
                 warn!(number=block.number(), hash=?block.hash(), "Encountered invalid block");
             }
             ConsensusEngineEvent::BlockReceived(num_hash) => {
-                info!(number=num_hash.number, hash=?num_hash.hash, "Received block from consensus engine");
+                // info!(number=num_hash.number, hash=?num_hash.hash, "Received block from consensus engine");
             }
         }
     }
