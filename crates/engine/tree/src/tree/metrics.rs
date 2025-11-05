17f04f6d12745c261f79902edc4a43e7e6e6e4b8
joey <10592664+joey0612@users.noreply.github.com>
2025-11-05 11:17:22 +0800

    opt: parallel diff hash post state

--- crates/engine/tree/src/tree/metrics.rs
+++ crates/engine/tree/src/tree/metrics.rs
@@ -101,7 +101,7 @@ impl EngineApiMetrics {
                 if let Some(ref hash_post_state_tx) = hash_post_state_tx {
                     let new_transition_state = executor.evm_mut().db_mut().borrow_mut().transition_state.clone();
                     if let Some(new_transition_state) = new_transition_state {
-                        let (new_bundle_state, hashed_post_state) = diff_hashed_post_state(&bundle_state, &new_transition_state);
+                        let (new_bundle_state, hashed_post_state) = parallel_diff_hashed_post_state(&bundle_state, &new_transition_state);
                         hash_post_state_tx.send(MultiProofMessage::HashedPostStateUpdate(hashed_post_state)).unwrap();
                         bundle_state = new_bundle_state;
                     }
@@ -120,11 +120,10 @@ impl EngineApiMetrics {
         if let Some(hash_post_state_tx) = hash_post_state_tx_clone {
             let new_transition_state = db.borrow_mut().transition_state.clone();
             if let Some(new_transition_state) = new_transition_state {
-                info!(target: "engine::tree", "diff_hashed_post_state, transition_state={:?}, new_transition_state={:?}", bundle_state.state.len(), new_transition_state.transitions.len());
-                let (new_bundle_state, hashed_post_state) = diff_hashed_post_state(&bundle_state, &new_transition_state);
+                let (new_bundle_state, hashed_post_state) = parallel_diff_hashed_post_state(&bundle_state, &new_transition_state);
                 hash_post_state_tx.send(MultiProofMessage::HashedPostStateUpdate(hashed_post_state)).unwrap();
                 hash_post_state_tx.send(MultiProofMessage::FinishedStateUpdates).unwrap();
-                drop(hash_post_state_tx);
+                // drop(hash_post_state_tx);
                 bundle_state = new_bundle_state;
             }
         }
